5399b645eff5561ec1c6e0c83644c1647a0cff0b
Jerry Liu <jerryliu@appannie.com>
2015-12-02 18:48:42 +0800

    activate hot-reload

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -5,6 +5,21 @@ var baseConfs = require('./webpack.base.conf.js');
 baseConfs = lodash.map(baseConfs, function(conf) {
   conf.output.path = 'dist/dev';
 
+  conf.cache = true;
+  conf.devtool = 'eval';
+  conf.devServer = {
+    contentBase: './dist/dev',
+    port: 8080,
+    hot: true,
+    inline: true
+  };
+  lodash.map(conf.entry, function(entry, name) {
+    if (name.indexOf('app-') >= 0) {
+      entry.unshift('webpack/hot/dev-server');
+    }
+    return entry;
+  });
+
   return conf;
 });
 
